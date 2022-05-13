16f670bf0f0c9fb9f713de8715d90183da570560
Hung Thanh Bui <hungbui7690@gmail.com>
2022-05-13 08:23:23 +0700

    Finish Chapter Functions >> Now working on Arrays (starter)

--- Zero2Hero/06-Functions/09_more_closure_examples.js
+++ Zero2Hero/06-Functions/09_more_closure_examples.js
@@ -1,3 +1,8 @@
+//-------------------------------------------
+console.log("--------- 1 ----------");
+//-------------------------------------------
+
+// Example 1
 let f;
 
 const g = function () {
@@ -16,9 +21,28 @@ const h = function () {
 
 g();
 f();
-console.dir(f);
+console.dir(f); // closure: a = 4
 
 h();
 f();
 
 console.dir(f); // at this time, Closure just contains ONLY b = 10
+
+//-------------------------------------------
+console.log("--------- 2 ----------");
+//-------------------------------------------
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3; // if we comment this line >> perGroup = 1000 (line 47)
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each will ${perGroup} passengers`);
+  }, 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000; // this is to prove closure has higher priority than global scope chain
+boardPassengers(180, 3);
